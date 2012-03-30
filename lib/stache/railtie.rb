ba7d24cdf347ff961f4f4224be3daeea28bd8402
Thierry Zires <zshuaibin@gmail.com>
2012-03-30 10:32:52 +0800

    for some reason, i do not want use config.to_prepare by sending :append_view_path

--- lib/stache/railtie.rb
+++ lib/stache/railtie.rb
@@ -4,8 +4,8 @@ module Stache
       app.config.autoload_paths << Rails.root + 'app/views'
     end
 
-    config.to_prepare do
-      ApplicationController.send(:append_view_path, Stache.template_base_path)
-    end
+    # config.to_prepare do
+    #   ApplicationController.send(:append_view_path, Stache.template_base_path)
+    # end
   end
 end
